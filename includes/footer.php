2a57dddf67758306e1f501e9da5f63af08d811b8
himanshu-pise <161084519+himanshu-pise@users.noreply.github.com>
2024-04-05 20:02:47 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .footer-section {
+            position: fixed;
+            bottom: 1%;
+            width: 100%;
+            /*background-color: #22aff2; /* You can change the background color as needed */
+            text-align: center;
+            padding: 5px 5px; /* Adjust the top and bottom padding */
+        }
+        
+        /* Adjust margin for better spacing between elements */
+        .footer-section .row div {
+            margin: 10px 0px;
+        }
+    </style>
+</head>
+<body>
+
+<section class="footer-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                Exclusive Underground Marketplace
+            </div>
+            <div class="col-md-6">
+                Made with 💙 for students.
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Add more content here if needed -->
+
+</body>
+</html>
+
